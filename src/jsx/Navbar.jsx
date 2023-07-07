c71a97bbfb6eecc144185f038b822af170f34257
AaronBester <50926594+AaronBester-dev@users.noreply.github.com>
2023-07-06 21:52:40 -0400

    Added CellScale and started retooling everything

--- src/jsx/Navbar.jsx
+++ src/jsx/Navbar.jsx
@@ -3,15 +3,31 @@ import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Container } from "@mui/material";
 
 function Navbar() {
   return (
     <div className="Navbar">
       <div>
-        <Link to="/" className="navbutton">Home</Link>
-        <Link to="/cooperators" className="navbutton">Co-operators Work Report</Link>
-        <Link to="/ncr2021" className="navbutton">NCR 2021 Work Report</Link>
-        <Link to="/ncr2022" className="navbutton">NCR 2022 Work Report</Link>
+        <div className="container">
+          <div className="row" >
+            <div className="col">
+              <Link to="/" className="navbutton">Home</Link>
+            </div>
+            <div className="col">
+              <Link to="/cooperators" className="navbutton">Co-operators Work Report</Link>
+            </div>
+            <div className="col">
+              <Link to="/ncr2021" className="navbutton">NCR 2021 Work Report</Link>
+            </div>
+            <div className="col">
+              <Link to="/ncr2022" className="navbutton">NCR 2022 Work Report</Link>
+            </div>
+            <div className="col">
+              <Link to="/ncr2022" className="navbutton">CellScale Work Report</Link>
+            </div>
+          </div>
+        </div>
       </div>
       <br></br>
     </div>
